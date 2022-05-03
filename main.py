ccfa5e900fc43d16c85f1c667572c2b28d530843
Oren Kohavi <oren_kohavi@brown.edu>
2022-05-03 16:35:47 -0400

    improve image reconstruction, still fails on vertical images

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 #This file trains the neural network with the training data, and then tests the network
 
-from pickletools import uint8
+import tensorflow as tf
 import numpy as np
 import random
 import os
 from PIL import Image #install using 'pip install Pillow'
 from network import BlockCNN
 
-def main(train_inputs, train_labels, test_inputs, test_labels, num_epochs, batch_size):
-    create_img_output = True
-    #^ Set to True if you want to see the images in the output folder
-    #^ Currently, image reconstruction is super broken, so this is disabled
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #Suppress TensorFlow warnings
+
+def main(train_inputs_path, train_labels_path, test_inputs_path, test_labels_path, num_epochs, batch_size):
+    create_img_output = True #Enabling this will output the results of the network to the outputs folder
+    verbose_img_output = False #Enabling this will also output inputs and labels to the outputs folder
     
     #Load the data
-    train_inputs = np.load(train_inputs)
-    train_labels = np.load(train_labels)
-    test_inputs = np.load(test_inputs)
-    test_labels = np.load(test_labels)
+    train_inputs = np.load(train_inputs_path)
+    train_labels = np.load(train_labels_path)
+    test_inputs = np.load(test_inputs_path)
+    test_labels = np.load(test_labels_path)
 
-    #Shuffle the data
+    #Shuffle the training data
     temp = list(zip(train_inputs, train_labels))
     random.shuffle(temp)
     ti, tl = zip(*temp)
     shuffled_train_inputs, shuffled_train_labels = np.array(list(ti)), np.array(list(tl))
 
+    #Shuffle the testing data
     temp = list(zip(test_inputs, test_labels))
     random.shuffle(temp)
     ti, tl = zip(*temp)
     shuffled_test_inputs, shuffled_test_labels = np.array(list(ti)), np.array(list(tl))
 
-    all_inputs = np.concatenate((test_inputs, train_inputs))
-    all_labels = np.concatenate((test_labels, train_labels))
-    del(train_inputs, train_labels, test_inputs, test_labels) #free up memory, since these are huge arrays
+    if create_img_output:
+        #Create the output folder
+        if not os.path.isdir("./output"):
+            os.mkdir("./output")
+        #Create arrays with original (non-shuffled) inputs and labels for reconstruction later
+        all_inputs = np.concatenate((test_inputs, train_inputs))
+        all_labels = np.concatenate((test_labels, train_labels))
+
+    #free up memory, since these are huge arrays and are no longer needed
+    del(train_inputs, train_labels, test_inputs, test_labels)
 
     input_shape=(24,24,3)
     label_shape=(8,8,3)
@@ -40,43 +49,56 @@ def main(train_inputs, train_labels, test_inputs, test_labels, num_epochs, batch
     assert shuffled_train_labels.shape[1:] == label_shape
     assert shuffled_test_inputs.shape[1:] == input_shape
     assert shuffled_test_labels.shape[1:] == label_shape
+    print("Data loaded successfully")
 
-    model = BlockCNN()
-    model.compile(optimizer='adam', loss=model.loss)
+    print(f"Building model and training for {num_epochs} epochs")
+    model = BlockCNN() #Create our model
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss=model.loss)
     model.fit(shuffled_train_inputs, shuffled_train_labels, epochs=num_epochs, batch_size=batch_size)
-    #The model is now trained!
+    print("Training complete")
     #Now, run the model on the test data
     loss = model.evaluate(shuffled_test_inputs, shuffled_test_labels)
     print(f"Model loss: {loss}")
-    #Create some human-viewable examples with the non-shuffled data
+
     if create_img_output:
-        image_output_size = (768,512)
+        print("Creating Image Output... This may take a few seconds")
+        #Create some human-viewable examples with the non-shuffled data
+        image_output_shape = (768,512,3)
+        pixels_per_image = image_output_shape[0]*image_output_shape[1]
         tile_size = label_shape[0]
-        assert (image_output_size[0]*image_output_size[1]) % (tile_size*tile_size) == 0
-        num_tiles_per_image = (image_output_size[0]*image_output_size[1]) // (tile_size*tile_size)
-        which_image = 17
-        one_image = all_inputs[which_image*num_tiles_per_image:(which_image+1)*num_tiles_per_image]
-        one_image_label = all_labels[which_image*num_tiles_per_image:(which_image+1)*num_tiles_per_image]
-        label_img = np_to_img(one_image_label, (768,512,3))
-        input_img = np_to_img(one_image, (768,512,3), crop_center=True)
-        one_image = model.predict(one_image)
-        output_img = np_to_img(one_image, (768,512,3))
-        if not os.path.isdir("./output"):
-            os.mkdir("./output")
-        label_img.save("./output/nn_label.png")
-        input_img.save("./output/nn_input.png")
-        output_img.save("./output/nn_output.png")
-        print("Done! 🥳🎉")
+        #Make sure that the image output size is divisible by the tile size
+        assert image_output_shape[0] % tile_size == 0
+        assert image_output_shape[1] % tile_size == 0
+        num_tiles_per_image = pixels_per_image // (tile_size*tile_size)
+        #Make sure that the number of total tiles is divisible by the number of tiles per image
+        assert all_inputs.shape[0] % num_tiles_per_image == 0
+        assert all_labels.shape[0] % num_tiles_per_image == 0
+        images = np.split(all_inputs, all_inputs.shape[0]//num_tiles_per_image)
+        if verbose_img_output:
+            #Also create images of the input and label
+            labels = np.split(all_labels, all_labels.shape[0]//num_tiles_per_image)
+            for i in range(len(images)):
+                pil_img = np_to_img(images[i], image_output_shape, crop_center=True)
+                pil_label = np_to_img(labels[i], image_output_shape)
+                pil_img.save(f"./output/{i}_input.png")
+                pil_label.save(f"./output/{i}_label.png")
+        #Create images of the output after running it through the model
+        for i in range(len(images)):
+            model_prediction = model.predict(images[i])
+            pil_prediction = np_to_img(model_prediction, image_output_shape)
+            pil_prediction.save(f"./output/{i}_prediction.png")
+    print("Done! 🥳🎉")
 
 def np_to_img(np_array, output_shape, crop_center=False, step=8):
     #Converts a np array to PIL image
-    #crop_center should be true when the np_array should be cropped to the tile size (hardcoded to 8x8)
+    #crop_center should be true when the np_array should be cropped to the tile size (used for the input images)
     if crop_center:
         num_tiles = (output_shape[0]*output_shape[1]) // (step*step)
-        new_np_array = np.zeros(shape=(num_tiles,step,step,3)) #Hardcoded for 3 channels
+        new_np_array = np.zeros(shape=(num_tiles,step,step,output_shape[-1]))
         for i in range(len(np_array)):
             new_np_array[i] = np_array[i][step:2*step, step:2*step, :]
         np_array = new_np_array
+    
     img_width = output_shape[0]
     img_height = output_shape[1]
     img_array = np.zeros(shape=output_shape)
